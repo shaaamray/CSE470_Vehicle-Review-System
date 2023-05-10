596bee7cbe679bd40a9b711e16cd837ce02b8d4a
shaaamray <83807318+shaaamray@users.noreply.github.com>
2023-05-10 20:27:15 +0600

    Update upload.js

--- frontend/src/upload.js
+++ frontend/src/upload.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-nEF7pJkkMSpqJkT4jNycYgQjWCOVBFs",
-  authDomain: "vehicle-review-system.firebaseapp.com",
-  projectId: "vehicle-review-system",
-  storageBucket: "vehicle-review-system.appspot.com",
-  messagingSenderId: "1082676741088",
-  appId: "1:1082676741088:web:7a59c7ba31f84de65ae185"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
